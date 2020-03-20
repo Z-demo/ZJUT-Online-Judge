fa1bf525750a222de0d99b1e91b722b05bf8694b
Z-demo <32590329+Z-demo@users.noreply.github.com>
2020-03-20 15:48:21 +0800

    Update 1002.道路.cpp

--- 校外_C++/1002.道路.cpp
+++ 校外_C++/1002.道路.cpp
@@ -1,36 +1,29 @@
 #include <iostream>
-#include <vector>
-#include <queue>
-#include <string>
-#include <sstream>
-#include <functional>
-
+#include<algorithm>
 using namespace std;
+bool cmpGreater(int a, int b) {
+    return a < b;
+}
+void solve_1002() {
+    int t, n;
+    const int maxn = 1000;
+    int a[maxn] = {0};//道路长度
+    cin >> t;
+    for (int i = 0; i < t; ++i) {//t组数据
+        cin >> n;//城市个数
+        int k=0,result=0;
+        for (int j = 0; j < n - 1; ++j) {//n个城市 n-1次联通
+            cin >> k;//当前城市与下一城市有k条路径
+            for (int i = 0; i < k; ++i) cin >> a[i];
+            sort(a, a + k, cmpGreater);//升序
+            result += a[0] + a[1];//取最短的两条路径
+        }
+        cout << result << endl;
+    }
 
-int main() {
-	int t;
-	cin >> t;
-	for (int i = 0; i < t; i++) { //对每组数据
-		int n;
-		cin >> n;
-		vector<priority_queue<int, vector<int>, greater<int>>> road(n-1); //设置优先队列每次弹出最小值
-		for (int j = 0; j < road.size(); j++) { //对相邻城市间的道路
-			int amount;
-			cin >> amount;
-			for (int k = 0; k < amount; k++) { //对每条道路
-				int length;
-				cin >> length;
-				road[j].push(length);
-
-			}
-		}
-		
-		int total_length = 0;
-		for (int j = 0; j < road.size(); j++) { //每两个城市间，选择两条最短路径
-			total_length += road[j].top();
-			road[j].pop();
-			total_length += road[j].top();
-		}
-		cout << total_length << endl;
-	}	
+}
+int main()
+{
+    solve_1002();
+    return 0;
 }
